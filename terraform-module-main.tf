8ea6534740481d274775663f018158d744dfa36d
Ranjith Rajkumar <ranjith.rajkumar@hpe.com>
2020-05-21 13:42:39 -0500

    updating infra

---
+++ terraform-module-main.tf
@@ -0,0 +1,13 @@
+module "druble-instance" {
+    source = "./terraform-infra-resources/terraform-infra-ec2"    
+    ami           = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_name
+    subnet_id = var.subnet_id
+    tags = var.tags
+    security_group_name = var.security_group_name
+    vpc_id      = var.vpc_id
+    public_key = var.public_key
+}
+
+
